7105f85725a1a2db1d8f7560d11a5e2cab923bb2
Vova Batsyk <vovabatsyk@gmail.com>
2017-10-02 16:22:38 +0000

    “Initials”

---
+++ js/bootstrap-collapse-navbar.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('.navbar-nav>li>a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+});
+
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#smartphones article').hover( 
+        function() {$(this).addClass('animated shake')},
+        function() {$(this).removeClass('animated shake')} 
+    );
+});
+
