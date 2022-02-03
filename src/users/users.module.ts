23f08e6baff2e057b569136402677b63c10aacf1
vovabatsyk <vovabatsyk.lviv@gmail.com>
2022-02-03 22:54:59 +0200

    add: user role

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from './user.model';
+import { User } from './users.model';
+import { Role } from '../roles/roles.model';
+import { UserRoles } from '../user-roles/user-roles.model';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
-  imports: [
-    SequelizeModule.forFeature([User])
-  ]
+	controllers: [UsersController],
+	providers: [UsersService],
+	imports: [
+		SequelizeModule.forFeature([User, Role, UserRoles]),
+		RolesModule
+	],
 })
-export class UsersModule {}
+export class UsersModule {
+}
