f1e1b29c70cc20b9499713b749d57fba9a5b542e
vovabatsyk <vovabatsyk.lviv@gmail.com>
2022-02-03 19:58:46 +0200

    add: sequelize User model

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function start() {
 	const PORT = process.env.PORT || 5000
 	const app = await NestFactory.create(AppModule)
 
-	await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+	await app.listen(PORT, () =>
+		console.log(`Server started on port = ${PORT}`))
 }
 
 start()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
 export class UsersService {}
