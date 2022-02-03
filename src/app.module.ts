886f1cf866f324cddb40359d652904c590d0bd91
vovabatsyk <vovabatsyk.lviv@gmail.com>
2022-02-03 18:54:48 +0200

    fix:.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
 	controllers: [],
 	providers: [],
 	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${process.env.NODE_ENV}.env`
+		}),
 		SequelizeModule.forRoot({
 			dialect: 'postgres',
 			host: process.env.POSTGRES_HOST,
 			port: Number(process.env.POSTGRES_PORT),
-			username: 'postgres',
-			password: '4817',
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
 			database: process.env.POSTGRES_DB,
 			models: [],
 			autoLoadModels: true,
