431731ff40aefa7cc159d08f39cc0412d5f384cf
vovabatsyk <vovabatsyk.lviv@gmail.com>
2022-02-04 18:08:32 +0200

    user: add role & ban

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import { User } from './users.model';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Roles } from '../auth/roles-auth.decorator';
 import { RolesGuard } from '../auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 
 @ApiTags('Users')
 @Controller('users')
@@ -28,5 +30,25 @@ export class UsersController {
 	@Get()
 	getAll() {
 		return this.userService.getAllUsers();
+	}
+
+	@ApiOperation({ summary: 'Add role' })
+	@ApiResponse({ status: 200 })
+	@UseGuards(JwtAuthGuard)
+	@Roles('admin')
+	@UseGuards(RolesGuard)
+	@Post('/role')
+	addRole(@Body() dto: AddRoleDto) {
+		return this.userService.addRole(dto);
+	}
+
+	@ApiOperation({ summary: 'Ban user' })
+	@ApiResponse({ status: 200 })
+	@UseGuards(JwtAuthGuard)
+	@Roles('admin')
+	@UseGuards(RolesGuard)
+	@Post('/ban')
+	ban(@Body() dto: BanUserDto) {
+		return this.userService.ban(dto);
 	}
 }
