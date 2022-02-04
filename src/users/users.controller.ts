0dcda3cd6c371937a626a82dc5863fb22e99c872
vovabatsyk <vovabatsyk.lviv@gmail.com>
2022-02-04 17:48:24 +0200

    guard: roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles-auth.decorator';
+import { RolesGuard } from '../auth/roles.guard';
 
 @ApiTags('Users')
 @Controller('users')
@@ -21,6 +23,8 @@ export class UsersController {
 	@ApiOperation({ summary: 'Get all users' })
 	@ApiResponse({ status: 200, type: [User] })
 	@UseGuards(JwtAuthGuard)
+	@Roles('admin')
+	@UseGuards(RolesGuard)
 	@Get()
 	getAll() {
 		return this.userService.getAllUsers();
